a35583d9528bb0bf7ed4bc545f2f5326002c20a9
claudiadmr <claudiadm.ribeiro@hotmail.com>
2023-12-14 14:25:59 +0000

    removed project

--- CPILint_GA/backend/src/main/java/pt/CPILint_GA/backend/utils/ExceptionConstants.java
+++
@@ -1,7 +0,0 @@
-package pt.CPILint_GA.backend.utils;
-
-public final class ExceptionConstants {
-    public static final String REQUIRED = "is required.";
-
-    public static final String EMAIL_EXISTS = "Email already exists.";
-}
