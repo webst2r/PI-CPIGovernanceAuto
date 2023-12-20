f6f133eebd2766ce0de852bc3430f3a9856e2e41
Rodrigo Rodrigues <rodrigo.rodrigues@automaise.com>
2023-12-20 20:49:42 -0100

    Flows, Credentials, Login

--- Project/frontend/src/app/pages/credentials/credentials.component.ts
+++ Project/frontend/src/app/pages/credentials/credentials.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from "@angular/material/input";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { MatIconModule } from "@angular/material/icon";
+import { AppConstant } from "../../app.constant";
+import {MatButtonModule} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-credentials',
   standalone: true,
-  imports: [],
+  imports: [
+    MatInputModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterLink,
+    TranslateModule
+  ],
   templateUrl: './credentials.component.html',
-  styleUrl: './credentials.component.scss'
+  styleUrls: ['./credentials.component.scss']
 })
 export class CredentialsComponent {
+  form: FormGroup = new FormGroup({
+    baseUrl: new FormControl('', [Validators.required]),
+    tokenUrl: new FormControl('', [Validators.required]),
+    tokenId: new FormControl('', [Validators.required]),
+    clientId: new FormControl('', [Validators.required]),
+  });
 
+  registerCredentials() {
+    // Handle form submission logic here
+    if (this.form.valid) {
+      const formData = this.form.value;
+      console.log(formData); // You can use formData to send the data to your backend
+    }
+  }
 }
--- Project/frontend/src/app/pages/flows/flows.component.ts
+++ Project/frontend/src/app/pages/flows/flows.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from "@angular/material/table";
+
+export interface FlowElement {
+  position: number;
+  package: string;
+  flowId: number;
+  actions: string;
+}
+
+const FLOW_DATA: FlowElement[] = [
+  { position: 1, package: 'Package 1', flowId: 123, actions: 'Edit, Delete' },
+  { position: 2, package: 'Package 2', flowId: 456, actions: 'Edit, Delete' },
+  // Add more rows as needed
+];
 
 @Component({
   selector: 'app-flows',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTableModule
+  ],
   templateUrl: './flows.component.html',
-  styleUrl: './flows.component.scss'
+  styleUrls: ['./flows.component.scss'] // Fix the typo in styleUrls
 })
 export class FlowsComponent {
-
+  displayedColumns: string[] = ['position', 'package', 'flowId', 'actions'];
+  dataSource = FLOW_DATA;
 }
