464cc24874d5bb5b78db6b32ba98deb82c344efc
Rodrigo Rodrigues <rodrigo.rodrigues@automaise.com>
2024-01-02 22:27:44 -0100

    integração do github

--- Project/backend/src/main/java/org/project/backend/packages/PackagesController.java
+++ Project/backend/src/main/java/org/project/backend/packages/PackagesController.java
@@ -1,22 +1,32 @@
 package org.project.backend.packages;
 
+import lombok.extern.slf4j.Slf4j;
+import org.project.backend.github.GithubService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 @RestController
+@Slf4j
 @RequestMapping("/api/packages")
 public class PackagesController {
 
     private final PackagesService packagesService;
 
+    private final GithubService gitHubService;
+
     @Autowired
-    public PackagesController(PackagesService packagesService) {
+    public PackagesController(PackagesService packagesService, GithubService gitHubService) {
         this.packagesService = packagesService;
+        this.gitHubService = gitHubService;
     }
 
 
@@ -48,12 +58,26 @@ public class PackagesController {
         return ResponseEntity.ok(response);
     }
 
-
     @GetMapping("/downloadFlow/{id}/{version}")
-    public ResponseEntity<byte[]> downloadFlow( @PathVariable("id") String flowId, @PathVariable("version") String flowVersion) {
+    public ResponseEntity<byte[]> downloadFlow(@PathVariable("id") String flowId, @PathVariable("version") String flowVersion) throws IOException, InterruptedException {
+        // Fazer download do Flow para as transferências
         ResponseEntity<byte[]> response = packagesService.downloadFlow(flowId, flowVersion);
+
+
+        // Enviar o Flow para o GitHub
+
+        String branch = "teste";
+        String filePath = "teste4.txt";
+
+        try {
+            gitHubService.sendFlowToGitHub(branch, filePath);
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
         return ResponseEntity.ok(response.getBody());
     }
+
 
 
 
