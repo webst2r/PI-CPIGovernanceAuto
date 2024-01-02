77664f63dac3effbfe782083404302a616c8c3c1
Rodrigo Rodrigues <rodrigo.rodrigues@automaise.com>
2024-01-02 22:27:44 -0100

    integração do github

--- Project/backend/src/main/java/org/project/backend/credential/github/GithubCredentialsRepository.java
+++ Project/backend/src/main/java/org/project/backend/credential/github/GithubCredentialsRepository.java
@@ -1,5 +1,6 @@
 package org.project.backend.credential.github;
 
+import org.project.backend.credential.sap_cpi.CredentialSapCpi;
 import org.project.backend.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,4 +8,6 @@ import java.util.Optional;
 
 public interface GithubCredentialsRepository extends JpaRepository<GithubCredentials, Long> {
     Optional<GithubCredentials> findByUser(User user);
+
+    GithubCredentials findByUserId(Long id);
 }
