f39858b86ce0bc716156def8c521b4fbb92611c9
Rodrigo Rodrigues <rodrigo.rodrigues@automaise.com>
2024-01-17 20:20:31 -0100

    register/home page

--- Project/frontend/src/app/pages/home/home.component.ts
+++ Project/frontend/src/app/pages/home/home.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, computed, inject} from '@angular/core';
 import {TranslateModule} from "@ngx-translate/core";
+import {navbarData} from "../../sidenav/nav-data";
+import {RouterLink} from "@angular/router";
+import {AuthenticationService} from "../../services/authentication.service";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    TranslateModule
+    TranslateModule,
+    RouterLink,
+    NgForOf
   ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  private auth = inject(AuthenticationService)
+
+  navData = computed(() => {
+    return navbarData.filter((data) =>
+      data.authRequired === this.auth.isLoggedInSig())
+  });
+
 }
