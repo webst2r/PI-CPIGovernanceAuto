42981856cd7d1d8435ccd4c826dad3815efe3993
jtmr05 <62111562+jtmr05@users.noreply.github.com>
2024-01-23 17:40:27 +0000

    removed cpilint and pipeline

--- pipeline/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        JenkinsPipeline jenkinsPipeline = new JenkinsPipeline("11fa0ad06989c56774548a0aaaac927069","admin");
-        jenkinsPipeline.create("Hello", "file.xml");
-        jenkinsPipeline.execute("Hello");
-    }
-}
